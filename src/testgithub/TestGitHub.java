30afa452eafc6ed6d3fb9f85ef8733064de2db68
Khalid Joraid <joraidk@sheridancollege.ca>
2022-02-20 14:56:18 -0500

    Commit #1

---
+++ src/testgithub/TestGitHub.java
@@ -0,0 +1,17 @@
+
+package testgithub;
+
+/**
+ *
+ * @author Khalid Joraid
+ */
+public class TestGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+
+}
